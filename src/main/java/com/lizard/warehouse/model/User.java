1b304af1dc802005d06723da60c497370074d374
Hakan Kurt <hkurt@ebay.com>
2020-08-30 23:20:25 +0300

    Implement user class

---
+++ src/main/java/com/lizard/warehouse/model/User.java
@@ -0,0 +1,11 @@
+package com.lizard.warehouse.model;
+
+public class User {
+    private Long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
